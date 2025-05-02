daa80a161ba7afe28f831526a1b1b6d7b38e3df5
dilbar26 <dilbar.abdijalilova@gmail.com>
2025-05-02 19:17:37 +0500

    Update homework.py

--- lesson-1/homework/homework.py
+++ lesson-1/homework/homework.py
@@ -1,1 +1,38 @@
+# 1
 
+a = 5
+perimeter = 4 * a
+area = a ** 2
+
+
+print(perimeter)
+print(area)
+
+#2
+
+diameter = 20
+r = diameter/2
+P = 3.14
+length = 2*P*r
+
+print(length)
+
+#3  
+
+a = 1
+b = 3
+mean = (a+b)/2
+print (mean)
+
+#4
+
+a = 10
+b = 20
+sum = a+b
+product = a*b
+square_a = a **2
+square_b = b **2
+print (sum)
+print (product)
+print (square_a)
+print (square_b)
